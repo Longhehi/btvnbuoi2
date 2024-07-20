b98263a55097bb7a128dfb70bba33a637b40a898
Longhehi <dolong12ng13@gmail.com>
2024-07-20 21:55:34 +0700

    bai3.py

--- bai3.py
+++ bai3.py
@@ -1,17 +1,16 @@
-def triangle():
- a = int(input("Nhap canh thu nhat: "))
- b = int(input("Nhap canh thu hai: "))
- c = int(input("nhap canh thu ba: "))
- if a+b>c and a+c>b and b+c>a:
-     print("Ba canh nay tao thanh mot tam giac ")
-     if a == b == c:
-         print("Ba canh nay tao thanh mot tac giac deu ")
-     elif a==b or a==c or c==b:
-             print("Ba canh nay tao thanh mot tam giac can")
-     elif a*a + b*b == c*c or b*b + c*c == a*a or a*a + c*c == b*b:
-        print("Day la tam giac vuong ")
-     else:
-         print("Day la tam giac thuong")
- else:
-     print("ba canh tren khong the tao thanh mot tam giac")
-triangle()
+n = int(input("Nhap n: "))
+z = 0
+for i in range(1, 2 * n + 2):
+    if i % 2 == 0:
+        z -= i
+    else:
+        z += i
+print("tong_s1:", z)
+
+
+
+n = int(input(" nhap n:"))
+x = 0
+for i in range(1, n+1):
+    x += 1/i
+print("tong _s2:", x)
