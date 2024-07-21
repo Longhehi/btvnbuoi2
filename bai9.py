74f274f3bfee30ecb941ca3f52463cc468b82e9d
Longhehi <dolong12ng13@gmail.com>
2024-07-21 10:15:16 +0700

    bai9.py

---
+++ bai9.py
@@ -0,0 +1,9 @@
+a = int(input("Nhập vào số a: "))
+num_bits = a.bit_length()
+print(f"Số lượng các bits cần thiết để biểu diễn số {a} ở dạng nhị phân là: {num_bits}")
+
+
+x = "awesome"
+print("Python is " + x)
+print("Python is {}".format(x))
+print(f"Python is {x}")
